4ae09c35bd19cc7326fc52a24bd6c61605284706
Vitor <vitor.reis@crmservices.com.br>
2024-05-01 18:13:33 -0300

    feat: home page

---
+++ app/_components/products-list.tsx
@@ -0,0 +1,26 @@
+import { Prisma } from "@prisma/client";
+import ProductItem from "./product-item";
+
+interface ProductsListProps {
+  products: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
+  }>[];
+}
+
+const ProductsList = async ({ products }: ProductsListProps) => {
+  return (
+    <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductsList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,89 @@
-const Home = () => {
-  return <div className="mx-6 bg-red-500 py-5"></div>;
+import Header from "./_components/Header";
+import CategoryList from "./_components/category-list";
+import Search from "./_components/search";
+import ProductsList from "./_components/products-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
+import { db } from "./_lib/prisma";
+import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
+
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <Header />
+
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/promo-banner-01.svg"
+          alt="Até 30% de desconto em pizzas"
+        />
+      </div>
+
+      <div className="pt-6 space-y-4">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+
+          <Button
+            variant="ghost"
+            className="h-fit text-primary p-0 hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+
+        <ProductsList products={products} />
+      </div>
+
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/promo-banner-02.svg"
+          alt="A partir de R$17,90 em lanches"
+        />
+      </div>
+
+      <div className="py-6 space-y-4">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+
+          <Button
+            variant="ghost"
+            className="h-fit text-primary p-0 hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+
+        <RestaurantList />
+      </div>
+    </>
+  );
 };
 
 export default Home;
