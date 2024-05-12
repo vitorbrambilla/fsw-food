42950cf119eda7258645a977a96e142916db9da9
Vitor <vitor.reis@crmservices.com.br>
2024-05-11 22:12:50 -0300

    feat: restaurant list

--- app/restaurants/[id]/_components/cart-banner.tsx
+++ app/restaurants/[id]/_components/cart-banner.tsx
@@ -7,12 +7,11 @@ import {
   SheetContent,
   SheetHeader,
   SheetTitle,
-  SheetTrigger,
 } from "@/app/_components/ui/sheet";
 import { CartContext } from "@/app/_context/cart";
 import { formatCurrency } from "@/app/_helpers/price";
 import { Restaurant } from "@prisma/client";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 interface CartBannerProps {
   restaurant: Pick<Restaurant, "id">;
@@ -20,6 +19,7 @@ interface CartBannerProps {
 
 const CartBanner = ({ restaurant }: CartBannerProps) => {
   const { products, totalPrice, totalQuantity } = useContext(CartContext);
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
   const restaurantHasProductsOnCart = products.some(
     (product) => product.restaurantId === restaurant.id,
@@ -45,16 +45,15 @@ const CartBanner = ({ restaurant }: CartBannerProps) => {
         </div>
 
         {/* BOTÃO */}
-        <Sheet>
-          <SheetTrigger>
-            <Button>Ver sacola</Button>
-          </SheetTrigger>
+        <Button onClick={() => setIsCartOpen(true)}>Ver sacola</Button>
+
+        <Sheet open={isCartOpen} onOpenChange={setIsCartOpen}>
           <SheetContent className="w-[90vw]">
             <SheetHeader>
               <SheetTitle className="text-left">Sacola</SheetTitle>
             </SheetHeader>
 
-            <Cart setIsOpen={() => {}} />
+            <Cart setIsOpen={setIsCartOpen} />
           </SheetContent>
         </Sheet>
       </div>
